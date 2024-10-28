283bc69ddb5c8fe15a1aca3ce8b2963a20d6be67
Enno-W <ennomail95@yahoo.de>
2024-10-28 23:24:04 +0100

    I encountered a problem rendering cross-references

--- Script/Values+Tables.R
+++ Script/Values+Tables.R
@@ -2,4 +2,24 @@
 descriptives_list<-mean_sd_median_min_max(df)
 
 #demographics table
-demographicstable<- select(df, Age, Gender, Sport) %>% tbl_summary( percent = "column", by = Gender) %>%  add_p() %>% add_overall()
+demographicsdata<- select(df, Age, Gender, Sport) %>% tbl_summary( percent = "column", by = Gender) %>%  add_p() %>% add_overall()
+
+data <- as.data.frame(demographicsdata) %>%
+  rename_with(~ gsub("\\*\\*", "", .)) # To remove the asterisks from the headers. 
+
+#Creating the table like this does not allow for control of the row height. But if original APA format is needed, this is just right. 
+# data %>% 
+#    flextable() %>% 
+#    theme_apa() %>% 
+#    autofit()
+
+demographicstable<- data %>%
+  flextable() %>%
+  set_table_properties(layout = "autofit") %>%
+  height(height = 0.3, part = "body") %>%  # Set row height for body
+  height(height = 0.5, part = "header") %>% # Set row height for header
+  font(fontname = "Times New Roman", part = "all") %>%
+  fontsize(size = 12, part = "all") %>%
+  padding(padding.top = 5, padding.bottom = 5) %>%
+  align(align = "center", part = "header") %>%
+  align(align = "center", part = "body")
