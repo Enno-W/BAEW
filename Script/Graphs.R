e1b19d8e1409e13e6b1b3c95aa095adc7656c995
Enno-W <ennomail95@yahoo.de>
2024-11-02 14:56:29 +0100

    I added a graph to show the distribution of all variables

--- Script/Graphs.R
+++ Script/Graphs.R
@@ -1,1 +1,20 @@
-hist.WeeklyH_ave <- ggplot(df_imp)
+ggplot(df, aes(WeeklyKM_base))+
+  geom_histogram(aes(y=..density..), colour ="black", fill="white", bins = 20)+
+  labs(x = "Weekly KM Baseline", y = NULL)+
+scale_y_continuous(guide = "none")+
+  stat_function(fun = dnorm, args=list(mean =mean (df$WeeklyKM_base, na.rm = T ), sd= sd(df$WeeklyKM_base, na.rm=T)), colour="black", linewidth =1)
+
+df_long <- df %>%
+  pivot_longer(cols = where(is.numeric), names_to = "variable", values_to = "value")
+
+
+ggplot(df_long, aes(value)) +
+  geom_histogram(aes(y = ..density..), colour = "black", fill = "white", bins = 20) +
+  labs(x = NULL, y = NULL) +
+  scale_y_continuous(guide = "none") +
+  facet_wrap(~variable, scales = "free") + # Create separate panels for each variable
+  stat_function(fun = dnorm,
+                args = list(mean = mean(df_long$value, na.rm = TRUE),
+                            sd = sd(df_long$value, na.rm = TRUE)),
+                colour = "black", linewidth = 1)
+df %>% sum(is.finite(df))
