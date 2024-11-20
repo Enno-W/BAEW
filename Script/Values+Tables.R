a612bb1614ecff4b26da37ca0a0f5d0b2ac1f66e
Enno-W <enno_winkler@outlook.com>
2024-11-20 17:04:36 +0100

    - more advanced models

--- Script/Values+Tables.R
+++ Script/Values+Tables.R
@@ -54,16 +54,43 @@ long_df <- df %>%
   mutate(
     Time = as.numeric(Time)           # Convert extracted timepoint to numeric
   )
+long_df<-long_df %>% rename( PositiveAffect=PA,NegativeAffect="NA")
 # Test that the transformation worked as intended: We have 33 obs. in "df", and here, each individual gets 6 rows for each measurement point. 
 nrow(long_df)== nrow(df)*6
 df$Goal_5[1]==long_df$Goal[5]# The 5th measurement point of "Goal" is equal to the variable "Goal_5" of participant number 1 in the original df. 
 
 # Models
 # Nullmodell
-null_model<- lme(Goal ~ 1, data=long_df, random= ~1|ID, method="ML", na.action = na.omit) # See the documentation for lme: ?lme --> other options: na.exclude, na.pass...#
+null_model<- lme(Goal ~ 1, 
+                 data=long_df, 
+                 random= ~1|ID, 
+                 method="ML", 
+                 na.action = na.omit) # See the documentation for lme: ?lme --> other options: na.exclude, na.pass...#
 # The "1" stands for the "intercept"
 #The formula means: Fixed effects: for "Goal", only the intercepts are estimated. Random effects: "The intercept varies between participants". 
 summary(null_model)
 icc(nullmodell)# a very low ICC
-time_model <- lme(Goal ~ 1+ Time, data=long_df, random= ~1|ID, method="ML", na.action = na.omit)
+time_model <- lme(Goal ~ 1+ Time, 
+                  data=long_df, 
+                  random= ~1|ID, method="ML", 
+                  na.action = na.omit)
 summary(time_model)# This model barely fits better than the null model
+# Time varying covariates: Affect
+time_varying_model <- lme(Goal ~ 1+ Time+PositiveAffect+NegativeAffect, # This would mean I assume that the fluctuations of affect across time predict goal Achivement
+                  data=long_df, 
+                  random= ~1|ID, method="ML", 
+                  na.action = na.omit)
+time_varying_model$coefficients$fixed["PositiveAffect"]
+summary(time_varying_model)
+##Stable Covariates: Affect and Attribution
+time_varying_and_stable_model <- lme(Goal ~ 1+ Time+PositiveAffect+NegativeAffect+PA_base+NA_base+Locus+Dynamics, # This would mean I assume that the fluctuations of affect across time predict goal Achivement
+                          data=long_df, 
+                          random= ~1|ID, method="ML", 
+                          na.action = na.omit)
+summary(time_varying_and_stable_model)
+#Level 1 Residuals (aka, at the time level), have thus far been viewed as uncorrelated. Here I introduce a covariation matrix for them.
+time_varying_and_stable_model_with_covar <- lme(Goal ~ 1+ Time+PositiveAffect+NegativeAffect+PA_base+NA_base+Locus+Dynamics, # This would mean I assume that the fluctuations of affect across time predict goal Achivement
+                                     data=long_df, 
+                                     random= ~1|ID, method="ML", 
+                                     na.action = na.omit, correlation = corAR1(form = ~ Time|ID))# I am not using ~ 1|ID)), because the gap between measurement points was not uniform and some data are missing. 
+summary(time_varying_and_stable_model_with_covar)
