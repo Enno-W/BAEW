5b4f52b2e9a250ea0bb8409eee97e241848f3ed0
Enno-W <ennomail95@yahoo.de>
2024-12-17 22:11:35 +0100

    - isolated the imputed dataset - specifiec the predictive models according to the hypothesis - changed to a GLMM for the training adherence models, because count_completed is not a continuous variable

---
+++ Script/Data_Import_and_cleaning.R
@@ -0,0 +1,76 @@
+#### Import and data cleansing ####
+# The commands in this script lead to the data frame that is loaded from the script "Import+Bereinigung.R". This script is not used in the markdown file
+####Importing the dataset####
+load(magic_path("241021PRIMOCA_data.Rdata"))
+sum(is.na(df))
+#### counting excluded participants ####
+raw_data_n <-nrow(df)
+
+#### Removing Participants based on the filter varialbe "Programme"#####
+df_filtered<-df %>% filter(Programme == 1|is.na(Programme))
+filtered_n <-nrow(df_filtered)
+#### NAs and Outliers ####
+df_filtered$NA_amount <- rowSums(is.na(df_filtered))
+df_less_na<-df_filtered %>% filter(NA_amount<50)#removing participants with more than 50 missings
+na_removed_n <-nrow(df_less_na)
+df<-df_less_na
+#### Removing Variables that are not considered in this thesis ####
+df<- df %>% select(!matches("_fear|_hope|Achievement|Affiliation|Power|Programme|Pride|Hubris"))
+#### Grouping the different kinds of sports and goals ####
+df$Sport2 <- NA
+df$Sport2[19] <- "Laufen" # Here, I add a second sport for participant 19
+patterns <- c(  "Kraftsport" = "kraft",   "Laufen" = "lauf")
+df <- replace_patterns(df, "Sport", patterns)# See the script "Functions.R" to examine the function
+
+#### Deal with hyphenss in weekly_KM_base ####
+df <- handle_hyphen(df, "WeeklyKM_base")# See the script "Functions.R" to examine the function
+df <- df %>%
+  mutate(
+    WeeklyH_base = gsub(",", ".", WeeklyH_base)
+  )
+df <- handle_hyphen(df, "WeeklyH_base") # also a function from the function script, it averages two numbers out if the person wrote something like 40-45
+
+#### Means based on a string that appears in the variable name####
+df$Goal_ave <- mean_by_pattern(df, "goal")# see functions script
+df$Commit_ave <- mean_by_pattern(df, "commit")
+df$SessionKM_ave <- mean_by_pattern(df, "sessionkm")
+df$SesseionH_ave <- mean_by_pattern(df, "sessionh")
+df$SessionRPE_ave <- mean_by_pattern(df, "sessionrpe")
+df$PA_ave <- mean_by_pattern(df, "pa_")
+df$NA_ave <- mean_by_pattern(df,"na_")
+
+####rename the gender ####
+df <- df %>%
+  mutate(Gender = recode(Gender, "1" = "Männlich", "2" = "Weiblich", "3" = "Divers"))
+####change character varialbes to numeric####
+df$WeeklyKM_base<- as.numeric(df$WeeklyKM_base)
+df$WeeklyH_base<- as.numeric(df$WeeklyH_base)
+
+#### Handling NAs####
+df[df=="NaN"]<-NA
+
+#### Variable to count training sessions ####
+#function to return "No" if something is FALSE, and vice versa...
+is.training.completed <- function(x) {
+  if (is.na(x)) {
+    return("No!")
+  } else {
+    return("Yes!")
+  }
+}
+# Running that whole thing through a for loop
+for (i in 1:6) {
+  goal_col <- paste0("Goal_", i)
+  newvar_col <- paste0("complete.", i)
+  df[[newvar_col]] <- sapply(df[[goal_col]], FUN = is.training.completed)
+}
+# count the number of "Yes!"es in those
+df$completed_count<- apply(select(df, starts_with(match = "complete.")), 1, function(x) length(which(x=="Yes!"))) # the "1" stands for rows here. see https://stackoverflow.com/questions/24015557/count-occurrences-of-value-in-a-set-of-variables-in-r-per-row
+
+# ID Variable
+df$ID<-1:nrow(df)
+
+####Multiple imputation####
+#imp <- mice(df, m=5, maxit=5, method="pmm") # number of multiple imputations, maximum iterations, method: predictive mean matching
+# https://bookdown.org/mwheymans/bookmi/multiple-imputation.html#multiple-imputation-in-r
+#df<-complete(imp)
--- Script/Values+Tables.R
+++ Script/Values+Tables.R
@@ -1,5 +1,8 @@
-#### Creating a list with all commonly used descriptive statistics####
+#### Creating a list with all commonly used descriptive statistics + other descriptive values####
 descriptives_list<-mean_sd_median_min_max(df)
+
+stat.desc(long_df$Goal_ave, basic = F, norm = T)
+df
 
 #### counting excluded participants ####
 raw_data_n <-nrow(df)
@@ -62,3 +65,128 @@ for (i in 1:6) {
 }
 # count the number of "Yes!"es in those
 df$completed_count<- apply(select(df, starts_with(match = "complete_")), 1, function(x) length(which(x=="Yes!"))) # the "1" stands for rows here. see https://stackoverflow.com/questions/24015557/count-occurrences-of-value-in-a-set-of-variables-in-r-per-row
+
+#### Multilevel Analysis ####
+# Nullmodell
+null_model<- lme(Goal ~ 1, 
+                 data=long_df, 
+                 random= ~1|ID, 
+                 method="ML", 
+                 na.action = na.omit) # See the documentation for lme: ?lme --> other options: na.exclude, na.pass...#
+# The "1" stands for the "intercept"
+#The formula means: Fixed effects: for "Goal", only the intercepts are estimated. Random effects: "The intercept varies between participants". 
+summary(null_model)
+icc(null_model) # The ICC is a lot lower with multiple imputation
+#### Model with fixed and Random effects
+#Center the time varying predictors to disentangle the repeated measurements from PA and NA traits
+long_df$PositiveAffect_centered <- long_df$PositiveAffect - ave(long_df$PositiveAffect, long_df$ID, FUN = mean)
+long_df$NegativeAffect_centered <- long_df$NegativeAffect - ave(long_df$NegativeAffect, long_df$ID, FUN = mean)
+
+#Hypothesis 1.1
+h1.1_model <- glmer(completed_count ~ 1 + Time + Locus + Dynamics + (1 | ID), 
+                    data = long_df, 
+                    family = "poisson")
+summary(h1.1_model)
+
+
+# Hypothesis 2.1
+h2.1_model <- glmer(completed_count ~ 1 + Time + NA_base + NegativeAffect_centered + (1 | ID), 
+                    data = long_df, 
+                    family = "poisson")
+summary(h2.1_model)
+
+
+# Hypothesis 1.2
+goal_model <- lme(Goal ~ 1+ Time+Locus+Dynamics, # This would mean I assume that the fluctuations of affect across time predict goal Achivement
+                  data=long_df, 
+                  random= ~ 1 |ID, method="ML", # The "|" sort of means "group by"
+                  na.action = na.omit, correlation = corAR1(form = ~ Time|ID))# I am not using ~ 1|ID)), because the gap between measurement points was not uniform and some data are missing. 
+summary(goal_model)
+
+goal_model <- lme(Goal ~ 1+ Time+NA_base+NegativeAffect_centered, # This would mean I assume that the fluctuations of affect across time predict goal Achivement
+                  data=long_df, 
+                  random= ~ 1 |ID, method="ML", # The "|" sort of means "group by"
+                  na.action = na.omit, correlation = corAR1(form = ~ Time|ID))# I am not using ~ 1|ID)), because the gap between measurement points was not uniform and some data are missing. 
+summary(goal_model)
+
+#Hypothesis 2.2 + 1.2
+goal_model <- lme(Goal ~ 1+ Time+Locus+Dynamics+NA_base+NegativeAffect_centered, # This would mean I assume that the fluctuations of affect across time predict goal Achivement
+                  data=long_df, 
+                  random= ~ 1 |ID, method="ML", # The "|" sort of means "group by"
+                  na.action = na.omit, correlation = corAR1(form = ~ Time|ID))# I am not using ~ 1|ID)), because the gap between measurement points was not uniform and some data are missing. 
+summary(goal_model)
+
+##### Checking Assumptions http://www.regorz-statistik.de/inhalte/r_HLM_2.html#####
+l1_residuals <- hlm_resid(commitment_model, level=1) # Funktion aus HLMdiag-Package
+#Now, I use the ".ls.resid" to make a graph. these are the "Least squares residuals", and they have the advantage that influences from level 2 and 1 are not mixed up. 
+ggplot(data = l1_residuals, aes(.ls.resid)) +
+  geom_histogram(aes(y = after_stat(density)), bins=10) +
+  stat_function(fun = dnorm,
+                args = list(mean = mean(l1_residuals$.ls.resid),
+                            sd = sd(l1_residuals$.ls.resid)), size=2) # Seems pretty normal to me. 
+# Shapiro test of normality
+shapiro.test(l1_residuals$.ls.resid)# not normally distributed after removing Participants with too many NaS
+# Testing for homoscedasticity: The varianz of residuals must be constant for all values
+ggplot(data=l1_residuals, aes(x=.ls.fitted, y=.ls.resid)) +
+  geom_point() # Weird linear patterns emerge: This means, homoscedasticity is violated. 
+# Outliers
+ggplot(data = l1_residuals, aes(y= .ls.resid)) + theme_gray() + geom_boxplot()#overall
+ggplot(data = l1_residuals, aes( x= .ls.resid, y= as.factor(ID))) + theme_gray() + geom_boxplot() # per group, the outliers are pretty random and the width varies substantially between groups
+#### Überprüfung der gewöhnlichen, nicht least squares Residuen#####
+ggplot(data = l1_residuals, aes(.resid)) +
+  geom_histogram(aes(y = after_stat(density)), bins=10) +
+  stat_function(fun = dnorm,
+                args = list(mean = mean(l1_residuals$.resid),
+                            sd = sd(l1_residuals$.resid)), size=2)
+shapiro.test(l1_residuals$.resid) # not normally distributed
+# Homoskedastizität
+ggplot(data=l1_residuals, aes(x=.fitted, y=.resid)) +
+  geom_point()
+qqmath(~resid(commitment_model), # Interesting, especially low values of "goal" are messy
+       type = c("p", "g"), 
+       xlab = "Theoretical Quantiles", 
+       ylab = "Standardized Residuals")
+# Ausreißer
+ggplot(data = l1_residuals, aes(y= .resid)) + theme_gray() + geom_boxplot()
+ggplot(data = l1_residuals, aes(x= .resid, y= as.factor(ID))) + theme_gray() + geom_boxplot()
+# Assumptions of normality and homoscedasticity are violated. Possible solutions could come from "robustlmm" package - to deal with "contamination", or using robust standard errors with "clubSandwich" and lme4
+#### Model for "goal" ####
+goal_model <- lme(Goal ~ 1+ Time+PA_base+NA_base+Locus+Dynamics+PositiveAffect_centered+NegativeAffect_centered, # This would mean I assume that the fluctuations of affect across time predict goal Achivement
+                  data=long_df, 
+                  random= ~ 1 |ID, method="ML", # The "|" sort of means "group by"
+                  na.action = na.omit, correlation = corAR1(form = ~ Time|ID))# I am not using ~ 1|ID)), because the gap between measurement points was not uniform and some data are missing. 
+summary(goal_model)
+##### Checking Assumptions http://www.regorz-statistik.de/inhalte/r_HLM_2.html#####
+l1_residuals <- hlm_resid(goal_model, level=1) # Funktion aus HLMdiag-Package
+#Now, I use the ".ls.resid" to make a graph. these are the "Least squares residuals", and they have the advantage that influences from level 2 and 1 are not mixed up. 
+ggplot(data = l1_residuals, aes(.ls.resid)) +
+  geom_histogram(aes(y = after_stat(density)), bins=10) +
+  stat_function(fun = dnorm,
+                args = list(mean = mean(l1_residuals$.ls.resid),
+                            sd = sd(l1_residuals$.ls.resid)), size=2) # Seems pretty normal to me. 
+# Shapiro test of normality
+shapiro.test(l1_residuals$.ls.resid)# not normally distributed after removing Participants with too many NaS
+# Testing for homoscedasticity: The varianz of residuals must be constant for all values
+ggplot(data=l1_residuals, aes(x=.ls.fitted, y=.ls.resid)) +
+  geom_point() # Weird linear patterns emerge: This means, homoscedasticity is violated. 
+# Outliers
+ggplot(data = l1_residuals, aes(y= .ls.resid)) + theme_gray() + geom_boxplot()#overall
+ggplot(data = l1_residuals, aes( x= .ls.resid, y= as.factor(ID))) + theme_gray() + geom_boxplot() # per group, the outliers are pretty random and the width varies substantially between groups
+#### Überprüfung der gewöhnlichen, nicht least squares Residuen#####
+ggplot(data = l1_residuals, aes(.resid)) +
+  geom_histogram(aes(y = after_stat(density)), bins=10) +
+  stat_function(fun = dnorm,
+                args = list(mean = mean(l1_residuals$.resid),
+                            sd = sd(l1_residuals$.resid)), size=2)
+shapiro.test(l1_residuals$.resid) # not normally distributed
+# Homoskedastizität
+ggplot(data=l1_residuals, aes(x=.fitted, y=.resid)) +
+  geom_point()
+qqmath(~resid(commitment_model), # Interesting, especially low values of "goal" are messy
+       type = c("p", "g"), 
+       xlab = "Theoretical Quantiles", 
+       ylab = "Standardized Residuals")
+# Ausreißer
+ggplot(data = l1_residuals, aes(y= .resid)) + theme_gray() + geom_boxplot()
+ggplot(data = l1_residuals, aes(x= .resid, y= as.factor(ID))) + theme_gray() + geom_boxplot()
+# Assumptions of normality and homoscedasticity are violated. Possible solutions could come from "robustlmm" package - to deal with "contamination", or using robust standard errors with "clubSandwich" and lme4
