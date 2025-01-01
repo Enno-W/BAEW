49f756982c628f01cf4ff70b94b86266a740d714
Enno-W <ennomail95@yahoo.de>
2025-01-01 20:48:35 +0100

    1. changed hlm to allow random slopes 2. improved HLM tables 3. added text and p-values under "hierarchische modelle"

--- Script/Values+Tables.R
+++ Script/Values+Tables.R
@@ -73,16 +73,6 @@ pwr_result <- pwr.r.test(n = NULL,
 
 #
 #### Multilevel Analysis ####
-# Nullmodell
-null_model_goal<- lme(Goal ~ 1, 
-                 data=long_df, 
-                 random= ~1|ID, 
-                 method="ML", 
-                 na.action = na.omit) # See the documentation for lme: ?lme --> other options: na.exclude, na.pass...#
-# The "1" stands for the "intercept"
-#The formula means: Fixed effects: for "Goal", only the intercepts are estimated. Random effects: "The intercept varies between participants". 
-summary(null_model_goal)
-icc_goal<--icc(null_model_goal) # The ICC is a lot lower with multiple imputation
 
 null_model_sessions<- lme(completed_count ~ 1, 
                       data=long_df, 
@@ -91,8 +81,17 @@ null_model_sessions<- lme(completed_count ~ 1,
                       na.action = na.omit) 
 
 summary(null_model_sessions)
-icc(null_model_sessions)
 
+# Nullmodell
+null_model_goal<- lme(Goal ~ 1, 
+                      data=long_df, 
+                      random= ~1|ID, 
+                      method="ML", 
+                      na.action = na.omit) # See the documentation for lme: ?lme --> other options: na.exclude, na.pass...#
+# The "1" stands for the "intercept"
+#The formula means: Fixed effects: for "Goal", only the intercepts are estimated. Random effects: "The intercept varies between participants". 
+summary(null_model_goal)
+icc_goal<--icc(null_model_goal) # The ICC is a lot lower with multiple imputation
 #### Model with fixed and Random effects
 #Center the time varying predictors to disentangle the repeated measurements from PA and NA traits
 long_df$PositiveAffect_centered <- long_df$PositiveAffect - ave(long_df$PositiveAffect, long_df$ID, FUN = mean)
@@ -103,106 +102,86 @@ h1.1_model <- glmer(completed_count ~ 1 + Time + Locus + Dynamics + (1 | ID),
                     data = long_df, 
                     family = "poisson")
 summary(h1.1_model)
-h1.1_table<-huxreg(h1.1_model, statistics = c("N" = "nobs", "R2" = "r.squared"))
+
 # Hypothesis 2.1
 h2.1_model <- glmer(completed_count ~ 1 + Time + NA_base + NegativeAffect_centered + (1 | ID), 
                     data = long_df, 
                     family = "poisson")
 summary(h2.1_model)
 
-
+hlmtable1<-huxreg("Nullmodell" = null_model_sessions, "Attribution-Modell (H 1.1)" = h1.1_model, "Affekt-Modell (H 2.1)" = h2.1_model ,statistics = NULL, number_format = 3, bold_signif = 0.05) 
 # Hypothesis 1.2
 goal_model1 <- lme(Goal ~ 1+ Time+Locus+Dynamics, # This would mean I assume that the fluctuations of affect across time predict goal Achivement
                   data=long_df, 
-                  random= ~ 1 |ID, method="ML", # The "|" sort of means "group by"
+                  random= ~ Time |ID, method="ML", # The "|" sort of means "group by"
                   na.action = na.omit, correlation = corAR1(form = ~ Time|ID))# I am not using ~ 1|ID)), because the gap between measurement points was not uniform and some data are missing. 
 summary(goal_model1)
 
 #Hypothesis 2.2
 goal_model2 <- lme(Goal ~ 1+ Time+NA_base+NegativeAffect_centered, # This would mean I assume that the fluctuations of affect across time predict goal Achivement
                   data=long_df, 
-                  random= ~ 1 |ID, method="ML", # The "|" sort of means "group by"
+                  random= ~ Time |ID, method="ML", # The "|" sort of means "group by"
                   na.action = na.omit, correlation = corAR1(form = ~ Time|ID))# I am not using ~ 1|ID)), because the gap between measurement points was not uniform and some data are missing. 
 summary(goal_model2)
+
+hlmtable2<-huxreg("Nullmodell" = null_model_goal, "Attribution-Modell (H 1.1)" = goal_model1, "Affekt-Modell (H 2.1)" = goal_model2 ,statistics = NULL, number_format = 3, bold_signif = 0.05)
 
 #Hypothesis 2.2 + 1.2
 goal_model3 <- lme(Goal ~ 1+ Time+Locus+Dynamics+NA_base+NegativeAffect_centered, # This would mean I assume that the fluctuations of affect across time predict goal Achivement
                   data=long_df, 
-                  random= ~ 1 |ID, method="ML", # The "|" sort of means "group by"
+                  random= ~ Time |ID, method="ML", # The "|" sort of means "group by"
                   na.action = na.omit, correlation = corAR1(form = ~ Time|ID))# I am not using ~ 1|ID)), because the gap between measurement points was not uniform and some data are missing. 
 summary(goal_model3)
 
 ##### Checking Assumptions http://www.regorz-statistik.de/inhalte/r_HLM_2.html#####
-l1_residuals <- hlm_resid(goal_model3, level=1) # Funktion aus HLMdiag-Package
+goal1_residuals <- hlm_resid(goal_model1, level=1) # Funktion aus HLMdiag-Package
 #Now, I use the ".ls.resid" to make a graph. these are the "Least squares residuals", and they have the advantage that influences from level 2 and 1 are not mixed up. 
-ggplot(data = l1_residuals, aes(.ls.resid)) +
+ggplot(data = goal1_residuals, aes(.ls.resid)) +
   geom_histogram(aes(y = after_stat(density)), bins=10) +
   stat_function(fun = dnorm,
-                args = list(mean = mean(l1_residuals$.ls.resid),
-                            sd = sd(l1_residuals$.ls.resid)), size=2) # Seems pretty normal to me. 
-# Shapiro test of normality
-shapiro.test(l1_residuals$.ls.resid)# not normally distributed after removing Participants with too many NaS
+                args = list(mean = mean(goal1_residuals$.ls.resid),
+                            sd = sd(goal1_residuals$.ls.resid)), size=2) # Seems pretty normal to me. 
+######Shapiro test of normality#######
+shaptest_goal_attrib <-shapiro.test(goal1_residuals$.ls.resid)# not normally distributed after removing Participants with too many NaS
+shaptest_goal_attrib_p <-if (shaptest_goal_attrib$p.value < 0.001) {
+  "< .001"
+} else {
+  round( shaptest_goal_affect$p.value, 3)
+}
+goal2_residuals <- hlm_resid(goal_model2, level=1)
+
+
+shaptest_goal_affect<-shapiro.test(goal1_residuals$.ls.resid)
+
+shaptest_goal_affect_p <-if (shaptest_goal_affect$p.value < 0.001) {
+  "< .001"
+} else {
+  round( shaptest_goal_affect$p.value, 3)
+}
 # Testing for homoscedasticity: The varianz of residuals must be constant for all values
-ggplot(data=l1_residuals, aes(x=.ls.fitted, y=.ls.resid)) +
+ggplot(data=goal1_residuals, aes(x=.ls.fitted, y=.ls.resid)) +
   geom_point() # Weird linear patterns emerge: This means, homoscedasticity is violated. 
 # Outliers
-ggplot(data = l1_residuals, aes(y= .ls.resid)) + theme_gray() + geom_boxplot()#overall
-ggplot(data = l1_residuals, aes( x= .ls.resid, y= as.factor(ID))) + theme_gray() + geom_boxplot() # per group, the outliers are pretty random and the width varies substantially between groups
-#### Überprüfung der gewöhnlichen, nicht least squares Residuen#####
-ggplot(data = l1_residuals, aes(.resid)) +
-  geom_histogram(aes(y = after_stat(density)), bins=10) +
-  stat_function(fun = dnorm,
-                args = list(mean = mean(l1_residuals$.resid),
-                            sd = sd(l1_residuals$.resid)), size=2)
-shapiro.test(l1_residuals$.resid) # not normally distributed
+ggplot(data = goal1_residuals, aes(y= .ls.resid)) + theme_gray() + geom_boxplot() #overall
+ggplot(data = goal1_residuals, aes( x= .ls.resid, y= as.factor(ID))) + theme_gray() + geom_boxplot() # per group, the outliers are pretty random and the width varies substantially between groups
+
 # Homoskedastizität
-ggplot(data=l1_residuals, aes(x=.fitted, y=.resid)) +
+ggplot(data=goal1_residuals, aes(x=.fitted, y=.resid)) +
   geom_point()
 qqmath(~resid(goal_model3), # Interesting, especially low values of "goal" are messy
        type = c("p", "g"), 
        xlab = "Theoretical Quantiles", 
        ylab = "Standardized Residuals")
+
+
 # Ausreißer
-ggplot(data = l1_residuals, aes(y= .resid)) + theme_gray() + geom_boxplot()
-ggplot(data = l1_residuals, aes(x= .resid, y= as.factor(ID))) + theme_gray() + geom_boxplot()
+ggplot(data = goal1_residuals, aes(y= .resid)) + theme_gray() + geom_boxplot()
+ggplot(data = goal1_residuals, aes(x= .resid, y= as.factor(ID))) + theme_gray() + geom_boxplot()
 # Assumptions of normality and homoscedasticity are violated. Possible solutions could come from "robustlmm" package - to deal with "contamination", or using robust standard errors with "clubSandwich" and lme4
-#### Model for "goal" ####
-goal_model <- lme(Goal ~ 1+ Time+PA_base+NA_base+Locus+Dynamics+PositiveAffect_centered+NegativeAffect_centered, # This would mean I assume that the fluctuations of affect across time predict goal Achivement
-                  data=long_df, 
-                  random= ~ 1 |ID, method="ML", # The "|" sort of means "group by"
-                  na.action = na.omit, correlation = corAR1(form = ~ Time|ID))# I am not using ~ 1|ID)), because the gap between measurement points was not uniform and some data are missing. 
-summary(goal_model)
-##### Checking Assumptions http://www.regorz-statistik.de/inhalte/r_HLM_2.html#####
-l1_residuals <- hlm_resid(goal_model, level=1) # Funktion aus HLMdiag-Package
-#Now, I use the ".ls.resid" to make a graph. these are the "Least squares residuals", and they have the advantage that influences from level 2 and 1 are not mixed up. 
-ggplot(data = l1_residuals, aes(.ls.resid)) +
-  geom_histogram(aes(y = after_stat(density)), bins=10) +
-  stat_function(fun = dnorm,
-                args = list(mean = mean(l1_residuals$.ls.resid),
-                            sd = sd(l1_residuals$.ls.resid)), size=2) # Seems pretty normal to me. 
-# Shapiro test of normality
-shapiro.test(l1_residuals$.ls.resid)# not normally distributed after removing Participants with too many NaS
-# Testing for homoscedasticity: The varianz of residuals must be constant for all values
-ggplot(data=l1_residuals, aes(x=.ls.fitted, y=.ls.resid)) +
-  geom_point() # Weird linear patterns emerge: This means, homoscedasticity is violated. 
-# Outliers
-ggplot(data = l1_residuals, aes(y= .ls.resid)) + theme_gray() + geom_boxplot()#overall
-ggplot(data = l1_residuals, aes( x= .ls.resid, y= as.factor(ID))) + theme_gray() + geom_boxplot() # per group, the outliers are pretty random and the width varies substantially between groups
-#### Überprüfung der gewöhnlichen, nicht least squares Residuen#####
-ggplot(data = l1_residuals, aes(.resid)) +
-  geom_histogram(aes(y = after_stat(density)), bins=10) +
-  stat_function(fun = dnorm,
-                args = list(mean = mean(l1_residuals$.resid),
-                            sd = sd(l1_residuals$.resid)), size=2)
-shapiro.test(l1_residuals$.resid) # not normally distributed
-# Homoskedastizität
-ggplot(data=l1_residuals, aes(x=.fitted, y=.resid)) +
-  geom_point()
-qqmath(~resid(goal_model3), # Interesting, especially low values of "goal" are messy
-       type = c("p", "g"), 
-       xlab = "Theoretical Quantiles", 
-       ylab = "Standardized Residuals")
+
+
 # Ausreißer
-ggplot(data = l1_residuals, aes(y= .resid)) + theme_gray() + geom_boxplot()
-ggplot(data = l1_residuals, aes(x= .resid, y= as.factor(ID))) + theme_gray() + geom_boxplot()
+ggplot(data = goal1_residuals, aes(y= .resid)) + theme_gray() + geom_boxplot()
+ggplot(data = goal1_residuals, aes(x= .resid, y= as.factor(ID))) + theme_gray() + geom_boxplot()
 # Assumptions of normality and homoscedasticity are violated. Possible solutions could come from "robustlmm" package - to deal with "contamination", or using robust standard errors with "clubSandwich" and lme4
+
